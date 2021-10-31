c8418204a105d7255227788ce757261561ed3d41
Cornelius Amzar <cornelius@amzar.de>
2021-10-31 10:13:17 +0100

    Renamed everything from Piwik to Matomo

--- plugin.php
+++ plugin.php
@@ -1,13 +1,14 @@
 <?php
 
-class pluginPiwikAnalytics extends Plugin {
+class pluginMatomoAnalytics extends Plugin {
 
         public function init()
         {
-                $this->dbFields = array(
-                        'piwik-fqdn'=>'',
-                        'site-id'=>''
-                );
+                $this->dbFields = []
+                        'matomo-fqdn'=>'',
+                        'site-id'   =>'',
+                        'token'     => ''
+                ];
         }
 
         public function form()
@@ -15,15 +16,15 @@ class pluginPiwikAnalytics extends Plugin {
                 global $Language;
 
                 $html  = '<div>';
-                $html .= '<label for="jspiwik-fqdn">'.$Language->get('Piwik Server FQDN').'</label>';
-                $html .= '<input id="jspiwik-fqdn" type="text" name="piwik-fqdn" value="'.$this->getDbField('piwik-fqdn').'">';
-                $html .= '<div class="tip">'.$Language->get('complete-this-field-with-the-piwik-server-fqdn').'</div>';
+                $html .= '<label for="jsmatomo-fqdn">'.$Language->get('Matomo Server FQDN').'</label>';
+                $html .= '<input id="jsmatomo-fqdn" type="text" name="matomo-fqdn" value="'.$this->getDbField('matomo-fqdn').'">';
+                $html .= '<div class="tip">'.$Language->get('complete-this-field-with-the-matomo-server-fqdn').'</div>';
                 $html .= '</div>';
 
                 $html .= '<div>';
-                $html .= '<label for="jssite-id">'.$Language->get('Piwik Site ID').'</label>';
+                $html .= '<label for="jssite-id">'.$Language->get('Matomo Site ID').'</label>';
                 $html .= '<input id="jssite-id" type="text" name="site-id" value="'.$this->getDbField('site-id').'">';
-                $html .= '<div class="tip">'.$Language->get('complete-this-field-with-the-piwik-site-id').'</div>';
+                $html .= '<div class="tip">'.$Language->get('complete-this-field-with-the-matomo-site-id').'</div>';
                 $html .= '</div>';
 
                 return $html;
@@ -32,20 +33,19 @@ class pluginPiwikAnalytics extends Plugin {
         public function siteHead()
         {
 
-                $html = PHP_EOL.'<!-- Piwik Analytics -->'.PHP_EOL;
+                $html = PHP_EOL.'<!-- Matomo Analytics -->'.PHP_EOL;
                 $html .= '<script type="text/javascript">'.PHP_EOL;
                 $html .= 'var _paq = _paq || [];'.PHP_EOL;
                 $html .= '_paq.push([\'trackPageView\']);'.PHP_EOL;
                 $html .= '_paq.push([\'enableLinkTracking\']);'.PHP_EOL;
                 $html .= '(function() {'.PHP_EOL;
-                $html .= '  var u="//'.htmlentities($this->getDbField('piwik-fqdn')).'/";'.PHP_EOL;
-                $html .= '  _paq.push([\'setTrackerUrl\', u+\'piwik.php\']);'.PHP_EOL;
+                $html .= '  var u="//'.htmlentities($this->getDbField('matomo-fqdn')).'/";'.PHP_EOL;
+                $html .= '  _paq.push([\'setTrackerUrl\', u+\'matomo.php\']);'.PHP_EOL;
                 $html .= '  _paq.push([\'setSiteId\', \''.htmlentities($this->getDbField('site-id')).'\']);'.PHP_EOL;
-                $html .= '  var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];'.PHP_EOL; 
-                $html .= '  g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'piwik.js\'; s.parentNode.insertBefore(g,s);'.PHP_EOL;
-		$html .= '})();'.PHP_EOL.'</script>'.PHP_EOL;
-                $html .= '<!-- End Piwik Code -->'.PHP_EOL;
+                $html .= '  var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];'.PHP_EOL;
+                $html .= '  g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'matomo.js\'; s.parentNode.insertBefore(g,s);'.PHP_EOL;
+		        $html .= '})();'.PHP_EOL.'</script>'.PHP_EOL;
+                $html .= '<!-- End Matomo Code -->'.PHP_EOL;
                 return $html;
         }
 }
-
